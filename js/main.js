8d5addaabdc93d231ca93956abe8f3db22a406b2
bokovium <bokyuri95@gmail.com>
2024-12-07 01:39:21 +0300

    удаляет лишнее из main

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
-import './data.js';
-import './functions.js';
-import './creating-miniature.js';
-import './big-picture.js';
-import './validation.js';
-import './scale-photo.js';
-import './photo-filters.js';
+
 import { getData } from './server.js';
 import { renderPhotos } from './creating-miniature.js';
 import { setupMiniatures } from './big-picture.js';
